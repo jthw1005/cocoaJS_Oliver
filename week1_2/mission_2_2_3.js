499255cf85286ac59d66950d7c8e11f910dc7258
jthw1005 <skl0105@naver.com>
2021-11-05 15:28:06 +0900

    week1_2에 mission_2_2_3 수정

--- week1_2/mission_2_2_3.js
+++ week1_2/mission_2_2_3.js
@@ -4,18 +4,26 @@
 const grades = [[88,76,77], [33,44,44], [90,100,94], [30,44,98]];
 const adder = (previousValue, currentValue) => previousValue + currentValue;
 
-function average(grades) {
-    // 각 학생들의 평균 점수    
+// 각 학생들의 평균 점수    
+function avgAll(grades) {
     for(let i = 0; i < grades.length; i++)
         console.log(`학생 ${i}의 평균 점수: ` + (grades[i].reduce(adder) / 3).toFixed(1));
+}
 
-    // 최고점수의 평균 점수
+// 최고점수의 평균 점수
+function avgHighest(grades) {
     let avgMax = 0;
 
     for (let i = 0; i < grades.length; i++)
         avgMax += Math.max(...grades[i]);
 
     console.log(`최고 점수의 평균점수: ${avgMax / 4}`);
+}
+
+// main.js
+function average(grades) {
+    avgAll(grades);    
+    avgHighest(grades);
 }
 
 average(grades);
