6d2fcda6d61689328eab2d05426a13f49b9c4090
jthw1005 <skl0105@naver.com>
2021-11-14 15:08:05 +0900

    week2_2 mission1 complete(for a single digit)

--- week2_2/mission_1.js
+++ week2_2/mission_1.js
@@ -1,68 +1,82 @@
-/*
-# 과제
-아래와 같은 배열 모양의 문자열을 파싱한다.
-이 때, 모든 숫자는 '[]'에 의해 감싸져 있다.
-const data = "[1,2,[3,4,[5,[6]]]]"
-
-# 과제 조건
-0. 객체 분석 정보를 출력한다.(깊이, 원소 개수)
-ex) "배열의 중첩된 깊이 수준은 4이며, 총 6개의 원소가 포함되어 있습니다."
-1. 괄호의 짝이 맞지 않는 경우 오류내용을 출력한다.
-ex) "닫는 괄호가 일치하지 않습니다."
-2. 배열 분석 정보를 출력한다.
-ex)
-const data = "[1,2,[3,4,[5,[6]]]]";
-run(data) >
-  {
-    type: "root",
-    child: [
-      {
-        type: "array",
-        child: [
-          {
-            type: "number",
-            value: "1",
-            child: [],
-          },
-          {
-            type: "array",
-            child: [
-              {
-                type: "number",
-                value: "2",
-                child: [],
-              },
-              {
-                type: "array",
-                child: [
-                  {
-                    type: "number",
-                    value: "3",
-                    child: [],
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-    ],
+const regExpForOpenBracket = /\[/m;
+  const regExpForCloseBracket = /\]/m;
+  const regExpForNumbers = /[0-9]/m;
+  const regExpForComma = /\,/m;
+  const data = "[1, 2, [3]]"
+  
+  class ArrInfo {
+      constructor(type, value, child) {
+          this.type = type;
+          this.value = value;
+          this.child = child;
+      }
   };
-
-# 힌트
-0. 문제를 단순화하여 해결해본다.
-1. 무한 중첩된 해법이 어렵다면, 특정한 배열문자열 형태를 처리하는 로직을 개발한다.
-2. 문자열을 iteration하며 의미 있는 토큰을 추출하고 이를 분석한다.
-3. 무한한 형태를 파싱할 때는, 데이터 파싱을 하는 단계에서, stack 자료 구조를 활용할 수 있다.
-ex) '['을 찾은 후에 ']'를 찾을 때까지 스택자료구조를 활용하여 데이터를 모은다.
-
-# 학습 포인트
-0. 문자열을 반복문을 이용하여 파싱하는 방법을 안다.
-1. 정규표현식이 무엇인지 안다.
-2. stack과 queue의 차이점과 활용점을 안다.
-3. 무한중첩된 구조를 재귀와 스택 자료구조를 활용하여 분석할 수 있다.
-
-# 코드 설계
-0. '['의 개수?
-1. 
-*/
+  
+  
+  function stackPointer(depthOfBracket) {
+      let currentStack = stack;
+      for(let i = 0; i < depthOfBracket; i++) {
+          currentStack = currentStack.child[currentStack.child.length - 1];
+      }
+      return currentStack;
+  }
+  
+  
+  /* Run */
+  function run(str) {
+  
+      const stack = {
+          type: "root",
+          child: [],
+      };
+      let numOfOpenBracket = 0;
+      let numOfCloseBracket = 0;
+      let numOfComma = 0;
+      let currentStack = stack;
+  
+  
+      for(let index = 0; index < str.length; index++){
+  
+          if(regExpForOpenBracket.test(str[index])) {
+              numOfOpenBracket++;
+              const newArrInfo = new ArrInfo("array","[",[])
+              currentStack.child.push(newArrInfo);
+  
+              currentStack = currentStack.child[currentStack.child.length - 1];
+          }
+  
+          else if(regExpForCloseBracket.test(str[index])) {
+              numOfCloseBracket++;
+  
+          }
+  
+          else if(regExpForNumbers.test(str[index])) {
+              const newArrInfo = new ArrInfo("number",str[index],[])
+              currentStack.child.push(newArrInfo);
+          }
+  
+          else if(regExpForComma.test(str[index])){
+              numOfComma++;
+          }
+  
+          else {
+              console.log("Error");
+          }
+      }  
+      
+  
+      if(numOfOpenBracket !== numOfCloseBracket) {
+          console.log("괄호가 서로 매칭되지 않습니다.");
+          return 1;
+      }
+      else {
+          console.log(`배열의 중첩된 깊이 수준은 ${numOfOpenBracket}이며, 총 ${numOfComma + 1}개의 원소가 포함되어 있습니다.`);
+      }
+  
+      return stack;
+  }
+  
+  
+  /* execution part */
+  
+  console.log(JSON.stringify(run(data),null,3));
--- week2_2/test.js
+++ week2_2/test.js
@@ -1,8 +1,72 @@
-const a = {
-    name: "dd",
-    age: 12
+const data1 = {
+type: "root",
+child: [
+    {
+    type: "array",
+    child: [
+        {
+        type: "number",
+        value: "1",
+        child: [],
+        },
+        {
+        type: "array",
+        child: [
+            {
+            type: "number",
+            value: "2",
+            child: [],
+            },
+            {
+            type: "array",
+            child: [
+                {
+                type: "number",
+                value: "3",
+                child: [],
+                },
+            ],
+            },
+        ],
+        },
+    ],
+    },
+],
 };
+// [1, 2, [3]]
+const stack = {
+    type: "root",
+    child: [
+        {
+        type: "array",
+        value: "[",
+        child: [
+            {
+                type: "number",
+                value: "1",
+                child: []
+            },
+            {
+                type: "number",
+                value: "2",
+                child: []
+            },
+            {
+                type: "array",
+                value: "[",
+                child: [
+                    {
+                        type: "number",
+                        value: "3",
+                        child: []
+                    }
+                ]
+            }
+        ]
+        }
+    ]
+    };
 
-a.push(123);
+data2.child.push({name: 'dd'});
+console.log(data2.child);
 
-console.log(a);
